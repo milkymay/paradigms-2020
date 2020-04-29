13fc70ce9a57342b28099f27a6906687d793322e
milkymay <missisPatrik@yandex.ru>
2020-04-29 20:45:47 +0300

    HW 8 HW 9 HW 10

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -1,1 +1,45 @@
-(ns expression)
+(defn abstractNaryOperation [func op & args]
+      (fn [vars]
+          (func op (map (fn [x] (x vars)) args))))
+
+(defn abstractUnaryOperation [f x]
+      (fn [vars]
+          (f (x vars))))
+
+(defn variable [name] (fn [vars] (vars name)))
+(defn constant [arg] (constantly arg))
+
+(def divide (partial abstractNaryOperation reduce (fn [a b] (/ a (double b)))))
+(def multiply (partial abstractNaryOperation apply *))
+(def subtract (partial abstractNaryOperation apply -))
+(def add (partial abstractNaryOperation apply +))
+;(def min (partial abstractNaryOperation reduce (fn [a b] (Math/min a b))))
+;(def max (partial abstractNaryOperation reduce (fn [a b] (Math/max a b))))
+(def negate (partial abstractUnaryOperation -))
+;(def exp (partial abstractUnaryOperation (fn [a] (Math/exp a))))
+;(def ln (partial abstractUnaryOperation (fn [a] (Math/ln a))))
+
+
+(def variables
+  {'x (variable "x")
+   'y (variable "y")
+   'z (variable "z")})
+
+(def operations
+  {'+      add
+   'negate negate
+   '-      subtract
+   '*      multiply
+   '/      divide})
+
+(defn parse [expr]
+      (cond
+        (list? expr)
+        (apply (operations (first expr))
+               (map parse (rest expr)))
+        (number? expr) (constant expr)
+        (contains? variables expr) (variables expr)
+        (contains? operations expr) (operations expr)))
+
+(defn parseFunction [expression]
+      (parse (read-string expression)))
